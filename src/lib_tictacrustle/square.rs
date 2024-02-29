5cd1a0c9e62b56d7ba4c68e0fb1939296aca8871
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-02-29 10:09:04 -0500

    docs(Square): add detailed docs to Square struct
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/lib_tictacrustle/square.rs
+++ src/lib_tictacrustle/square.rs
@@ -348,6 +348,12 @@ impl Display for Square {
     }
 }
 
+impl Display for Square {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
